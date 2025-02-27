1712e0e983956a6a5d22b37dc824ba3e4c95dc10
emirhantandogan <emirhantandogann@gmail.com>
2025-02-27 12:24:43 +0300

    ddqn103tests pth

--- Model/DDQNModel_1_0/dqn_agent.py
+++ Model/DDQNModel_1_0/dqn_agent.py
@@ -141,7 +141,7 @@ class DDQNAgent:
             #console print(f"It is the first turn of {self.color}ed DeepQNetworkAgent")
             self.first_turn = False
             action_params = {
-                "selected_destination_tickets": self.game_service.get_destination_tickets_list_at_the_start_of_the_game()}
+                "selected_destination_tickets": self.game_service.get_destination_tickets_list_at_the_start_of_the_game()[:2]}
             self.game_service.perform_action("draw_destination_ticket", action_params)
             return
 
